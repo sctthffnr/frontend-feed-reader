763cc55c4899c5e7b1e6b00c34b4ed76d2aab1ef
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-05-01 18:34:59 -0400

    Completed second test of RSS Feeds

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -32,6 +32,13 @@ $(function() {
          * and that the URL is not empty.
          */
 
+         it('has a defined URL', function() {
+           allFeeds.forEach(function(feed) {
+             expect(feed.url).toBeDefined();
+             expect(feed.url).not.toBeFalsy();
+           });
+         });
+
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
