f0fe7bae6a891f4e260f59cb74d91c2f4336db0a
Scott Heffner <scott.thomas.heffner@gmail.com>
2016-05-01 20:18:09 -0400

    Completed test for "Initial Entries"

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -75,6 +75,7 @@ $(function() {
         */
 
         it('changes visibility when clicked', function() {
+          $body = $('body');
           $('.menu-icon-link').click();
           expect($body.hasClass('menu-hidden')).toBe(false);
           $('.menu-icon-link').click();
@@ -85,18 +86,33 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+      beforeEach(function(done) {
+        loadFeed(0, function() {
+          done();
+        });
+      });
+      /* TODO: Write a test that ensures when the loadFeed
+       * function is called and completes its work, there is at least
+       * a single .entry element within the .feed container.
+       * Remember, loadFeed() is asynchronous so this test will require
+       * the use of Jasmine's beforeEach and asynchronous done() function.
+       */
+         it('ensures the loadFeed function completes', function(done) {
+            $feed = $('.feed').find('.entry');
+            expect($feed.length).toBeGreaterThan(0);
+            done();
+          });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+    });
+
+  /* TODO: Write a new test suite named "New Feed Selection"
+
+      /* TODO: Write a test that ensures when a new feed is loaded
+       * by the loadFeed function that the content actually changes.
+       * Remember, loadFeed() is asynchronous.
+       */
+
 }());
